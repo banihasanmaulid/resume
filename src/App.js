63f2df4d24fddffea4733a614b58d82d7070cbdc
banihasan <bani.maulid@indocyber.id>
2024-11-10 06:20:15 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
+import './App.module.scss';
+import BaseLayout from "./components/BaseLayout";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to My Home Page</h1>
-        <p>This is a simple homepage created with React.js.</p>
-        <button onClick={() => alert('Welcome!')}>Click Me</button>
-      </header>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <h1>Welcome to My Home Page</h1>
+    //     <p>This is a simple homepage created with React.js.</p>
+    //     <button onClick={() => alert('Welcome!')}>Click Me</button>
+    //   </header>
+    // </div>
+    <div>
+      <BrowserRouter basename="/resume">
+        <BaseLayout />
+      </BrowserRouter>
     </div>
   );
 }
